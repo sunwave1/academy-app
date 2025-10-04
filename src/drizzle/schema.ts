17ad147fcd9e068846d04f9e6081901316a0cf27
sunwave1 <fellipeanthony1@hotmail.com>
2025-10-03 23:10:21 -0300

    feat: database schema

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,48 @@
+import { sql } from 'drizzle-orm';
+import {
+  bigserial,
+  varchar,
+  text,
+  pgTable,
+  boolean,
+  timestamp,
+  uniqueIndex,
+  smallint,
+  bigint,
+  index,
+} from 'drizzle-orm/pg-core';
+
+export const users = pgTable(
+  'users',
+  {
+    id: bigserial({ mode: 'bigint' }).primaryKey(),
+    name: varchar('email', { length: 255 }),
+    email: varchar('email', { length: 350 }).unique(),
+    morph_id: bigint({ mode: 'bigint' }).default(sql`NULL`),
+    morph_model: varchar('morph_model', { length: 255 }).default(sql`NULL`),
+    age: smallint('age').default(sql`NULL`),
+    avatar_asset: text('avatar_asset').default(sql`NULL`),
+    password: varchar('password', { length: 355 }).notNull(),
+    cpf_cnpj: varchar('cpf_cnpj', { length: 255 }).default(sql`NULL`),
+    phone: varchar('phone', { length: 50 }).default(sql`NULL`),
+    second_phone: varchar('second_phone', { length: 50 }).default(sql`NULL`),
+    receive_marketing: boolean('receive_marketing').default(true),
+    deleted_at: timestamp('deleted_at').default(sql`NULL`),
+    birth_date_at: timestamp('birth_date_at').default(sql`NULL`),
+    created_at: timestamp('created_at').notNull().defaultNow(),
+    updated_at: timestamp('updated_at').notNull().defaultNow(),
+  },
+  (table) => [
+    uniqueIndex('email_idx').on(table.email),
+    index('name_idx').on(table.name),
+    index('phone_idx').on(table.phone),
+    index('second_phone_idx').on(table.second_phone),
+    index('receive_marketing_idx').on(table.receive_marketing),
+    index('deleted_at_idx').on(table.deleted_at),
+    index('birth_date_at_idx').on(table.birth_date_at),
+    index('created_at_idx').on(table.created_at),
+    index('morph_id_idx').on(table.morph_id),
+    index('morph_model_idx').on(table.morph_model),
+    index('cpf_cnpj_idx').on(table.cpf_cnpj),
+  ],
+);
