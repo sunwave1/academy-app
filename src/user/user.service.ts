ff883cba35e364703aff287bc2332c7bf0e389e5
sunwave1 <fellipeanthony1@hotmail.com>
2025-10-05 01:14:15 -0300

    feat: user service update and delete

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,6 +6,7 @@ import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from '../drizzle/schema';
 import { CreateUserDTO } from './dtos/Create';
 import { hash } from 'bcrypt';
+import { UpdateUserDTO } from './dtos/Update';
 
 @Injectable()
 export class UserService {
@@ -37,6 +38,33 @@ export class UserService {
     user.password = await hash(user.password, 8);
 
     const [result] = await this.db.insert(users).values(user).returning();
+
+    return result;
+  }
+  async Update(user: UpdateUserDTO) {
+    const [result] = await this.db
+      .update(users)
+      .set(user)
+      .where(eq(users.email, user.email))
+      .returning();
+    return result;
+  }
+  async Delete(id: number): Promise<{ id: number }> {
+    const exists = await this.db.query.users.findFirst({
+      columns: {
+        id: true,
+      },
+      where: eq(users.id, id),
+    });
+
+    if (!exists) {
+      throw new NotFoundException('Úsuario não encontrado');
+    }
+
+    const [result] = await this.db
+      .delete(users)
+      .where(eq(users.id, id))
+      .returning({ id: users.id });
 
     return result;
   }
