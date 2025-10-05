8717b3f6c95b766b5abd26a532c5be1e07d81a88
sunwave1 <fellipeanthony1@hotmail.com>
2025-10-05 20:16:33 -0300

    feat: table students and teachers, add column document_asset user

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -10,7 +10,14 @@ import {
   smallint,
   bigint,
   index,
+  integer,
 } from 'drizzle-orm/pg-core';
+
+const timestamps = {
+  deleted_at: timestamp('deleted_at').default(sql`NULL`),
+  created_at: timestamp('created_at').notNull().defaultNow(),
+  updated_at: timestamp('updated_at').notNull().defaultNow(),
+};
 
 export const users = pgTable(
   'users',
@@ -18,8 +25,6 @@ export const users = pgTable(
     id: bigserial({ mode: 'number' }).primaryKey(),
     name: varchar('name', { length: 255 }).notNull(),
     email: varchar('email', { length: 350 }).notNull().unique(),
-    morph_id: bigint({ mode: 'bigint' }).default(sql`NULL`),
-    morph_model: varchar('morph_model', { length: 255 }).default(sql`NULL`),
     age: smallint('age').default(sql`NULL`),
     avatar_asset: text('avatar_asset').default(sql`NULL`),
     password: varchar('password', { length: 355 }).notNull(),
@@ -27,10 +32,9 @@ export const users = pgTable(
     phone: varchar('phone', { length: 50 }).default(sql`NULL`),
     second_phone: varchar('second_phone', { length: 50 }).default(sql`NULL`),
     receive_marketing: boolean('receive_marketing').default(true),
-    deleted_at: timestamp('deleted_at').default(sql`NULL`),
     birth_date_at: timestamp('birth_date_at').default(sql`NULL`),
-    created_at: timestamp('created_at').notNull().defaultNow(),
-    updated_at: timestamp('updated_at').notNull().defaultNow(),
+    document_asset: text('document_asset').default(sql`NULL`),
+    ...timestamps,
   },
   (table) => [
     uniqueIndex('email_idx').on(table.email),
@@ -41,8 +45,20 @@ export const users = pgTable(
     index('deleted_at_idx').on(table.deleted_at),
     index('birth_date_at_idx').on(table.birth_date_at),
     index('created_at_idx').on(table.created_at),
-    index('morph_id_idx').on(table.morph_id),
-    index('morph_model_idx').on(table.morph_model),
     index('cpf_cnpj_idx').on(table.cpf_cnpj),
   ],
 );
+
+export const students = pgTable('students', {
+  id: bigserial({ mode: 'number' }).primaryKey(),
+  user_id: bigint({ mode: 'number' }).default(sql`NULL`),
+  enem_grade: integer('enem_grade').default(sql`NULL`),
+  ...timestamps,
+});
+
+export const teachers = pgTable('teachers', {
+  id: bigserial({ mode: 'number' }).primaryKey(),
+  user_id: bigint({ mode: 'number' }).default(sql`NULL`),
+  document: varchar('document', { length: 255 }).notNull().unique(),
+  ...timestamps,
+});
