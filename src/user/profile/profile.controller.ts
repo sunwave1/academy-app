439c1edf3110cd4b70d4a3869979602c1b37c6c6
sunwave1 <fellipeanthony1@hotmail.com>
2025-10-05 01:15:18 -0300

    refactor: change injection profile service to user service, and remove findOne and Create

--- src/user/profile/profile.controller.ts
+++ src/user/profile/profile.controller.ts
@@ -1,24 +1,16 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { ProfileUserService } from './profile.service';
-import { CreateUserDTO, UpdateUserDTO } from '../dtos/CreateUser';
+import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
+import { UpdateUserDTO } from '../dtos/Update';
+import { UserService } from '../user.service';
 
 @Controller('users')
 export class ProfileUserController {
-  constructor(private readonly userService: ProfileUserService) {}
+  constructor(private readonly userService: UserService) {}
 
-  @Post('create')
-  async Create(@Body() createUser: CreateUserDTO) {
-    return this.userService.Create(createUser);
-  }
   @Post('update')
   async Update(@Body() updateUser: UpdateUserDTO) {
     return this.userService.Update(updateUser);
   }
-  @Get(':id')
-  async FindOne(@Param('id') id: number) {
-    return this.userService.FindOne(id);
-  }
-  @Delete('/delete/:id')
+  @Delete('/delete')
   async Delete(@Param('id') id: number) {
     return this.userService.Delete(id);
   }
